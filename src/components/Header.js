ac0454f8c869b0a639133498780a7f7c4de85635
hns-dev <hekma.sh@gmial.com>
2021-07-23 14:16:25 +0300

    Style header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 const Header = ({ darkMode, handleDarkModeChange }) => {
   return (
-    <header>
-      <div>
-        <h1>Where in the world?</h1>
-        <button onClick={handleDarkModeChange}>
+    <header className="bg-white text-blue-dark dark:bg-blue-light dark:text-white shadow-sm py-8">
+      <div className="container flex justify-between items-center">
+        <h1 className="font-bold md:font-extrabold text-lg">
+          Where in the world?
+        </h1>
+        <button onClick={handleDarkModeChange} className="flex">
           {darkMode ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              className="ionicon w-4 h-4 fill-current"
               viewBox="0 0 512 512"
-              className="ionicon"
             >
               <title>Moon</title>
               <path d="M152.62 126.77c0-33 4.85-66.35 17.23-94.77C87.54 67.83 32 151.89 32 247.38 32 375.85 136.15 480 264.62 480c95.49 0 179.55-55.54 215.38-137.85-28.42 12.38-61.8 17.23-94.77 17.23-128.47 0-232.61-104.14-232.61-232.61z" />
@@ -16,7 +18,7 @@ const Header = ({ darkMode, handleDarkModeChange }) => {
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="ionicon"
+              className="ionicon w-4 h-4 fill-current"
               viewBox="0 0 512 512"
             >
               <title>Moon</title>
@@ -30,7 +32,9 @@ const Header = ({ darkMode, handleDarkModeChange }) => {
               />
             </svg>
           )}
-          <span>dark mode</span>
+          <span className="text-sm font-medium md:font-bold capitalize tracking-wide ml-2">
+            dark mode
+          </span>
         </button>
       </div>
     </header>
