0a6bf50184cb46ea21b6e672dbaa3ec232611816
hns-dev <hekma.sh@gmial.com>
2021-07-23 15:19:38 +0300

    Style search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,11 +5,11 @@ const Search = () => {
 
   return (
     <form>
-      <div>
+      <div className="w-full flex items-center bg-white text-gray-dark dark:bg-blue-light dark:text-white shadow-sm rounded-md px-6 py-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          className="ionicon w-5 h-5 mr-4 fill-current"
           viewBox="0 0 512 512"
-          className="ionicon"
         >
           <title>Search</title>
           <path d="M464 428L339.92 303.9a160.48 160.48 0 0030.72-94.58C370.64 120.37 298.27 48 209.32 48S48 120.37 48 209.32s72.37 161.32 161.32 161.32a160.48 160.48 0 0094.58-30.72L428 464zM209.32 319.69a110.38 110.38 0 11110.37-110.37 110.5 110.5 0 01-110.37 110.37z" />
@@ -20,7 +20,7 @@ const Search = () => {
           placeholder="Search for a country..."
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
-          autoFocus
+          className="w-full bg-transparent placeholder-gray-dark dark:placeholder-white py-2 px-3 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-dark dark:focus:ring-white"
         />
       </div>
     </form>
