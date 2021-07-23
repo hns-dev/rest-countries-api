2d9ad9e6ff27ece1866bb6fcf22dd88b39a0f5cf
hns-dev <hekma.sh@gmial.com>
2021-07-23 13:48:54 +0300

    Add dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    darkMode ? html.classList.add("dark") : html.classList.remove("dark");
+  }, [darkMode]);
+
+  const handleDarkModeChange = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header darkMode={darkMode} handleDarkModeChange={handleDarkModeChange} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,35 @@
-const Header = () => {
+const Header = ({ darkMode, handleDarkModeChange }) => {
   return (
     <header>
       <div>
         <h1>Where in the world?</h1>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="ionicon"
-            viewBox="0 0 512 512"
-          >
-            <title>Moon</title>
-            <path
-              d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="40"
-            />
-          </svg>
+        <button onClick={handleDarkModeChange}>
+          {darkMode ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="ionicon"
+            >
+              <title>Moon</title>
+              <path d="M152.62 126.77c0-33 4.85-66.35 17.23-94.77C87.54 67.83 32 151.89 32 247.38 32 375.85 136.15 480 264.62 480c95.49 0 179.55-55.54 215.38-137.85-28.42 12.38-61.8 17.23-94.77 17.23-128.47 0-232.61-104.14-232.61-232.61z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="ionicon"
+              viewBox="0 0 512 512"
+            >
+              <title>Moon</title>
+              <path
+                d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="40"
+              />
+            </svg>
+          )}
           <span>dark mode</span>
         </button>
       </div>
