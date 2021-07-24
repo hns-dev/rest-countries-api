aa624d791c95656a81d807735aa64490f1ef35e4
hns-dev <hekma.sh@gmial.com>
2021-07-25 00:07:27 +0300

    Fetch all countries from REST Countries API and save the result data in a state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Search from "./Search";
 import Filter from "./Filter";
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const url = "https://restcountries.eu/rest/v2/";
+
+    const fetchCountries = async () => {
+      const res = await axios(url);
+      setCountries(res.data);
+    };
+
+    fetchCountries();
+  });
+
   return (
     <main>
       <section className="container my-8 md:flex justify-between">
