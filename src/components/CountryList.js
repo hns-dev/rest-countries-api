ec40394318121a2f6713d0ff40766abd2c550f4d
hns-dev <hekma.sh@gmial.com>
2021-07-26 14:35:08 +0300

    Style country list container

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -14,7 +14,9 @@ const CountryList = ({ isLoading, countries }) => {
 
   return (
     // Display country list
-    <ul>{countryList}</ul>
+    <ul className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+      {countryList}
+    </ul>
   );
 };
 
