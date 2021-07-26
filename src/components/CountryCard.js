330cd7ddbb8e4549be783fc6e9c95a7fd430a874
hns-dev <hekma.sh@gmial.com>
2021-07-26 15:16:51 +0300

    Style country card

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,28 +1,34 @@
 const CountryCard = ({ country }) => {
   return (
-    <li>
+    <li className="bg-white dark:bg-blue-light shadow-md rounded-md overflow-hidden">
       {/* Flag img container */}
-      <div>
-        <img src={country.flag} alt={`${country.name} flag`} />
+      <div className="h-48">
+        <img
+          src={country.flag}
+          alt={`${country.name} flag`}
+          className="w-full h-full object-cover"
+        />
       </div>
       {/* Contry infos container */}
-      <div>
-        <h2>{country.name}</h2>
+      <div className="px-6 py-8 capitalize font-semibold leading-6">
+        <span className="mb-4 font-extrabold text-lg inline-block">
+          {country.name}
+        </span>
 
-        <span>
+        <div>
           <span>population: </span>
-          {country.population}
-        </span>
-        <br />
-        <span>
+          <span className="font-light">{country.population}</span>
+        </div>
+
+        <div>
           <span>region: </span>
-          {country.region}
-        </span>
-        <br />
-        <span>
+          <span className="font-light">{country.region}</span>
+        </div>
+
+        <div>
           <span>capital: </span>
-          {country.capital}
-        </span>
+          <span className="font-light">{country.capital}</span>
+        </div>
       </div>
     </li>
   );
