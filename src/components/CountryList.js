796ca1ae8986f8078753f717ad0372288a30e402
hns-dev <hekma.sh@gmial.com>
2021-07-26 15:48:45 +0300

    Clean country list structure and center loading spinner

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -3,20 +3,20 @@ import CountryCard from "./CountryCard";
 const CountryList = ({ isLoading, countries }) => {
   // When the fetching the data from API is done, loop through the countries and return a list item from CountryCard component
   const countryList = isLoading ? (
-    <div>
+    <div className="flex justify-center items-center mt-10">
       <img src="/img/spinner.svg" alt="" />
     </div>
   ) : (
-    countries.map((country) => (
-      <CountryCard key={country.name} country={country} />
-    ))
+    <ul className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+      {countries.map((country) => (
+        <CountryCard key={country.name} country={country} />
+      ))}
+    </ul>
   );
 
   return (
     // Display country list
-    <ul className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
-      {countryList}
-    </ul>
+    <>{countryList}</>
   );
 };
 
