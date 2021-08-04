85a2a4f41dc4aed4bcbb76e570a853e5b569e8f2
hns-dev <hekma.sh@gmial.com>
2021-08-05 01:20:05 +0300

    Creat loader component

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,11 +1,10 @@
 import CountryCard from "./CountryCard";
+import Loader from "./Loader";
 
 const CountryList = ({ isLoading, countries }) => {
   // When the fetching the data from API is done, loop through the countries and return a list item from CountryCard component
   const countryList = isLoading ? (
-    <div className="flex justify-center items-center mt-10">
-      <img src="/img/spinner.svg" alt="" />
-    </div>
+    <Loader />
   ) : (
     <ul className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
       {countries.map((country) => (
---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center mt-10">
+      <img src="/img/spinner.svg" alt="" />
+    </div>
+  );
+};
+
+export default Loader;
