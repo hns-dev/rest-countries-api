3d547839af44724773a0247b4c9ac84a2bb50698
hns-dev <hekma.sh@gmial.com>
2021-08-04 04:16:33 +0300

    Replace headless ui select with custom select menu

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,63 +1,65 @@
 import { useState } from "react";
-import { Listbox } from "@headlessui/react";
 
 const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
 
 const Filter = () => {
   const [selectedRegion, setSelectedRegion] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  // handle open state change
+  const handleOpenChange = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // handle option change
+  const handleOptionChange = (value) => {
+    setSelectedRegion(value);
+    setIsOpen(false);
+  };
 
   return (
-    <Listbox value={selectedRegion} onChange={setSelectedRegion}>
-      <div className="relative w-48 sm:w-60 lg:w-72 bg-transparent rounded-md shadow-sm font-medium">
-        <Listbox.Button className="w-full flex justify-between items-center shadow-sm text-left bg-white dark:bg-blue-light rounded-md px-5 py-4 focus:outline-none focus:ring-1 focus:ring-gray-dark dark:focus:ring-white">
-          <span className="font-semibold truncate">
-            {selectedRegion ? selectedRegion : "Filter by region"}
-          </span>
-          <span className="pointer-events-none">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              className="h-5 w-5"
-              fill="currentColor"
-              aria-hidden="true"
+    // Select menu container
+    <div className="relative w-48 sm:w-60 lg:w-72 bg-transparent rounded-md shadow-sm font-medium">
+      {/* select menu button */}
+      <button
+        onClick={handleOpenChange}
+        className="w-full flex justify-between items-center shadow-sm text-left bg-white dark:bg-blue-light rounded-md px-5 py-4 focus:outline-none focus:ring-1 focus:ring-gray-dark dark:focus:ring-white"
+      >
+        <span className="truncate">{selectedRegion || "Filter by region"}</span>
+        <span className="pointer-events-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            className="h-5 w-5"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
+      </button>
+
+      {/* Options container */}
+      {isOpen && (
+        <ul className="absolute w-full bg-white dark:bg-blue-light shadow-md rounded-md mt-2 p-1 focus:outline-none">
+          {regions.map((region) => (
+            <li
+              key={region}
+              onClick={() => handleOptionChange(region)}
+              className={`cursor-pointer select-none rounded-md px-5 py-2 hover:bg-gray-200 dark:hover:bg-blue-dark hover:bg-opacity-60 dark:hover:bg-opacity-50 ${
+                selectedRegion === region ? "font-semibold" : ""
+              }`}
             >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </span>
-        </Listbox.Button>
-        <Listbox.Options className="absolute w-full bg-white dark:bg-blue-light shadow-sm rounded-md mt-2 p-1 focus:outline-none ">
-          {regions.map((region, index) => (
-            <Listbox.Option
-              key={index}
-              value={region}
-              className={({ active }) =>
-                `${
-                  active
-                    ? "bg-gray-200 dark:bg-blue-dark bg-opacity-60 dark:bg-opacity-50 font-extrabold"
-                    : "font-semibold"
-                } cursor-default select-none rounded-md px-5 py-2`
-              }
-            >
-              {({ selected }) => (
-                <span
-                  className={`${
-                    selected
-                      ? "font-extrabold"
-                      : "text-blue-light dark:text-gray-light"
-                  } truncate`}
-                >
-                  {region}
-                </span>
-              )}
-            </Listbox.Option>
+              {region}
+            </li>
           ))}
-        </Listbox.Options>
-      </div>
-    </Listbox>
+        </ul>
+      )}
+    </div>
   );
 };
 
