0873b605c2c34aedbe5924dbc550f0bccfc37cea
hns-dev <hekma.sh@gmial.com>
2021-08-06 02:02:38 +0300

    Style NoCountry component

--- src/components/NoCountry.js
+++ src/components/NoCountry.js
@@ -1,6 +1,6 @@
 const NoCountry = () => {
   return (
-    <div>
+    <div className="mb-6 py-4 text-center bg-white dark:bg-blue-light rounded-md shadow-sm">
       <h2>No Countries Found</h2>
       <p>Please try a different search term</p>
     </div>
