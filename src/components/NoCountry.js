deb6e24685420edfac3cf2ade64d8a65461cac85
hns-dev <hekma.sh@gmial.com>
2021-08-05 22:46:49 +0300

    Create NoCountry component

---
+++ src/components/NoCountry.js
@@ -0,0 +1,10 @@
+const NoCountry = () => {
+  return (
+    <div>
+      <h2>No Countries Found</h2>
+      <p>Please try a different search term</p>
+    </div>
+  );
+};
+
+export default NoCountry;
