d37b8d9620f3b863886b8b5f64fdd67c913f309b
hns-dev <hekma.sh@gmial.com>
2021-08-20 15:26:58 +0300

    Virtualize list using react-render-if-visible

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,6 +1,7 @@
 import CountryCard from "./CountryCard";
 import Loader from "./Loader";
 import NoCountry from "./NoCountry";
+import RenderIfVisible from "react-render-if-visible";
 
 const CountryList = ({ countries, isLoading, error }) => {
   // When the fetching the data from API is done, loop through the countries and return a list item from CountryCard component
@@ -9,7 +10,9 @@ const CountryList = ({ countries, isLoading, error }) => {
   ) : (
     <ul className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
       {countries.map((country) => (
-        <CountryCard key={country.name} country={country} />
+        <RenderIfVisible defaultHeight={358} key={country.name}>
+          <CountryCard country={country} />
+        </RenderIfVisible>
       ))}
     </ul>
   );
